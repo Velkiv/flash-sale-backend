67c0b367bb85626ba47b15e8010c5027b165a169
Velkiv <kevinwalewangko078@gmail.com>
2025-12-30 13:42:30 +0800

    Menambahkan data dummy dan env.

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
-const port = 3000
-
+const port = process.env.port || 3000;
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send("Selamat datand di Rest API.")
+const product = [
+    { id: 1, name: 'smartphone Flagship', price: 15000000, current_stock: 45},
+    { id: 2, name: 'smartphone Lowship', price: 15000000, current_stock: 45},
+];
+
+const order = [
+    { id: 1, user_id: 123, product_id: 1 },
+    { id: 2, user_id: 123, product_id: 2 },
+];
+
+app.get('/product', (req, res) => {
+    res.json(product)
 })
 
+app.get('/order', (req, res) => {
+    res.json(order)
+})
+
+
 app.listen(port, ()=>{
-    console.log(`Sedang berjalan`)
+    console.log(`Sedang berjalan di port ${port}`)
 })
